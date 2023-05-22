6f592c8809c876edf82848822594749dc13a5716
2clubs <bennettdan85@gmail.com>
2023-05-22 16:18:13 -0400

    adds working edit form and submit button

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
-const Show = () => {
+const Show = (props) => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const customers = props.customers
+  const customer = customers ? customers.find( (p) => p._id === id ) : null
+
+  // state for form
+  const [ editForm, setEditForm ] = useState(customer)
+  // const [ isEditing, setIsEditing ] = useState(false)
+
+  // handleChange function
+  const handleChange = (event) => {
+    setEditForm({
+      ...editForm,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  // handleSubmit function
+  const handleUpdate = (event) => {
+    event.preventDefault()
+    props.updateCustomers(editForm, customer._id)
+  }
+
+  const loaded = () => {
+    return (
+      <>
+        <h1>{customer.name}</h1>
+        <h2>{customer.phoneNumber}</h2>
+      </>
+    )
+  }
+
+  const loading = () => {
+    return <h1>Loading...</h1>
+  }
+
   return (
-    <div>Show</div>
+    <div className='customer'>
+      { customer ? loaded() : loading() }
+
+      {/* { isEditing && */}
+        <form onSubmit={handleUpdate}>
+          <input
+            type='text'
+            value={editForm.name}
+            name='name'
+            placeholder='name'
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            value={editForm.phoneNumber}
+            name='phoneNumber'
+            placeholder='phoneNumber'
+            onChange={handleChange}
+          />
+          <input type='submit' value={'Update Customer'} />
+        </form>
+      {/* } */}
+    </div>
   )
 }
 
