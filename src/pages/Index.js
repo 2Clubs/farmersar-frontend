20ace5471e6f7d2fbf1b8dbdbec14c83624d362b
2clubs <bennettdan85@gmail.com>
2023-05-22 11:59:26 -0400

    adds index page maps customers

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Index = () => {
-  return (
-    <div>Index</div>
-  )
+const Index = (props) => {
+
+  // loaded function
+  const loaded = () => {
+    return props.customers.map((customer) => (
+      <div key={customer._id} className='customer'>
+        <Link to={`/customers/${customer._id}`}>
+          <h1>{customer.name}</h1>
+          <h2>{customer.phoneNumber}</h2>
+        </Link>
+      </div>
+    ))
+  }
+
+  const loading = () => {
+    return <h1>Loading...</h1>
+  }
+
+  return props.customers ? loaded() : loading()
 }
 
 export default Index
