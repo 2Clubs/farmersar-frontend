ce25d7d4f9a0b6299419a25e0ffa76c21e59b2d3
2clubs <bennettdan85@gmail.com>
2023-05-22 11:58:58 -0400

    adds main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Index from '../pages/Index'
+import Show from '../pages/Show'
 
-const Main = () => {
+const Main = (props) => {
+  const [ customers, setCustomers ] = useState(null)
+
+  const URL = 'http://localhost:4000/customers/'
+
+  const getCustomers = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    setCustomers(data)
+  }
+
+  const createCustomers = async (customer) => {
+    // make post request to create customers
+    await fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+      body: JSON.stringify(customer),
+    })
+    // update list of customers
+    getCustomers()
+  }
+
+  useEffect(() => {getCustomers()}, [])
+
   return (
-    <div>Main</div>
+    <main>
+      <Routes>
+        <Route exact path='/' element={<Index customers={customers} createCustomers={createCustomers}/>} />
+        <Route
+          path='/customers/:id'
+          element = {
+            <Show />
+          } />
+      </Routes>
+    </main>
   )
 }
 
